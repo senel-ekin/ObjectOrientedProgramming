0f11c7d2b69cc32bbaee4cc0fba2d95f1a4ec5fa
senel-ekin <s.ekin_ozekin@hotmail.com>
2023-10-31 20:58:37 +0300

    Done

--- Library/src/Librarian.java
+++ Library/src/Librarian.java
@@ -1,2 +1,23 @@
 public class Librarian {
+    public static void main(String[] args) {
+        Student     student;
+        LibraryCard card1, card2;
+
+        student = new Student();
+        student.setName("Ekin");
+        student.setEmail("ee@javauniv.edu");
+
+        card1 = new LibraryCard();
+        card1.setOwner(student);
+        card1.checkOut(3);
+
+        card2 = new LibraryCard();
+        card2.setOwner(student);  //the same student is the owner
+                                  //of the student card, too
+        System.out.println("Card1 Info: ");
+        System.out.println(card1.toString() + "\n");
+
+        System.out.println("Card2 Info: ");
+        System.out.println(card2.toString() + "\n");
+    }
 }
