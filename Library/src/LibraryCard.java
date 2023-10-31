184dec1ee9436b4bf403f450939d8bd7ef63edcf
senel-ekin <s.ekin_ozekin@hotmail.com>
2023-10-31 20:33:52 +0300

    Done

---
+++ Library/src/LibraryCard.java
@@ -0,0 +1,3 @@
+public class LibraryCard {
+    private Student owner;
+}
---
+++ Library/src/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private String name;
+    private String email;
+
+    public Student() {
+        name = "Unassigned";
+        email = "Unassigned";
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setEmail(String address) {
+        email = address;
+    }
+
+    public void setName(String studentName) {
+        name = studentName;
+    }
+}
