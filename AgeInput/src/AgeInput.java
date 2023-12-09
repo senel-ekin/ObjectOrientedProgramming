add17dd646cb435deb4c02d951b417d67715b659
senel-ekin <s.ekin_ozekin@hotmail.com>
2023-12-09 19:32:41 +0300

    find birth year

--- AgeInput/src/AgeInput.java
+++ AgeInput/src/AgeInput.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class AgeInput {
+    private static final String DEFAULT_MESSAGE = "Your age?";
+    private Scanner scanner;
+
+    public AgeInput() {
+        scanner = new Scanner(System.in);
+    }
+
+    public int getAge() {
+        return getAge(DEFAULT_MESSAGE);
+    }
+
+    public int getAge(String message) {
+        System.out.println(message);
+        int age = scanner.nextInt();
+        return age;
+    }
 }
--- AgeInput/src/AgeInputTest.java
+++ AgeInput/src/AgeInputTest.java
@@ -1,2 +1,27 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
+
 public class AgeInputTest {
+    public static void main(String[] args) {
+        GregorianCalendar today;
+        int age, thisYear, bornYear;
+        String answer;
+        Scanner scanner = new Scanner(System.in);
+        AgeInput input = new AgeInput();
+        age = input.getAge("How old are you?");
+
+        today = new GregorianCalendar();
+        thisYear = today.get(Calendar.YEAR);
+
+        bornYear = thisYear - age;
+
+        System.out.println("Already had your birthday this year? (Y or N)");
+        answer = scanner.next();
+
+        if (answer.equals("N") || answer.equals("n")) {
+            bornYear--;
+        }
+        System.out.println("\nYou are born in " + bornYear);
+    }
 }
