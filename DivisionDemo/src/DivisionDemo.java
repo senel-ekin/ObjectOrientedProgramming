aa57bbf40464d14ae31aa893a66969d9c502221c
senel-ekin <s.ekin_ozekin@hotmail.com>
2023-12-10 17:15:29 +0300

    zero exception

--- DivisionDemo/src/DivisionDemo.java
+++ DivisionDemo/src/DivisionDemo.java
@@ -10,13 +10,31 @@ public class DivisionDemo {
             int denominator = scanner.nextInt();
 
             if (denominator == 0)
-                throw new RuntimeException();
+                throw new DivisionByZeroException();
+
+//            if (denominator == 0)
+//                throw new RuntimeException();
 
             double quotient = numerator / (double) denominator;
             System.out.println(numerator + "/" + denominator + "=" + quotient);
 
-        } catch (RuntimeException e) {
+        } catch (DivisionByZeroException e) {
             System.out.println("Be sure the denominator is not zero.");
+            secondChance();
         }
+
+//        } catch (RuntimeException e) {
+//            System.out.println("Be sure the denominator is not zero."); }
+    }
+
+    public static void secondChance() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numerator: ");
+        int numerator = scanner.nextInt();
+        System.out.println("Enter denominator: ");
+        int denominator = scanner.nextInt();
+        double quotient = numerator / (double) denominator;
+        System.out.println(numerator + "/" + denominator + "=" + quotient);
+        System.out.println("Be sure the denominator is not zero.");
     }
 }
