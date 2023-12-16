1c745b17b7c25b5fc4ed35f760cbfcbd982428c3
senel-ekin <s.ekin_ozekin@hotmail.com>
2023-12-16 23:53:22 +0300

    Rectangle class added

--- GeometricObject/src/Test01.java
+++ GeometricObject/src/Test01.java
@@ -4,9 +4,10 @@ public class Test01 {
         System.out.println(c1.toString());
         System.out.println(c1.getPerimeter());
         c1.printCircle();
+        DisplayObject(new Circle(3,"red",true));
     }
 
-    public void DisplayObject(GeometricObject object) {
+    public static void DisplayObject(GeometricObject object) {
         System.out.println("Created on: " + object.getDateCreated()+
                 "\nColor of the object: " + object.getColor());
     }
