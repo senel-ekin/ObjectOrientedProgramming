4d70c1170401c2486d5ad2e41debdf318c03f84f
senel-ekin <s.ekin_ozekin@hotmail.com>
2023-12-24 18:39:53 +0300

    arrays

---
+++ Array/src/array.java
@@ -0,0 +1,27 @@
+public class array {
+    public static void main(String[] args) {
+        double[] array01 = new double[10];
+        for (int i = 0; i < array01.length; i++) {
+            array01[i] = Math.random()*100;
+            System.out.format("%5.2f  ", array01[i]);
+        }
+
+        System.out.println("");
+        double[] array02 = new double[10];
+        array02 = array01;
+        for (int i = 0; i < array01.length; i++) {
+            System.out.format("%5.2f  ", array02[i]);
+        }
+
+        System.out.println("");
+        array01[5]=1.11;
+        for (int i = 0; i < array01.length; i++) {
+            System.out.format("%5.2f  ", array01[i]);
+        }
+
+        System.out.println("");
+        for (int i = 0; i < array01.length; i++) {
+            System.out.format("%5.2f  ", array02[i]);
+        }
+    }
+}
