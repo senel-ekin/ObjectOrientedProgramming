401e4492e43dd7213620c3125ba97fb9d0973dd7
senel-ekin <s.ekin_ozekin@hotmail.com>
2023-12-24 18:07:14 +0300

    done

---
+++ IskambilKartlari/src/IskambilKarlari.java
@@ -0,0 +1,29 @@
+public class IskambilKarlari {
+    public static void main(String[] args) {
+        int[] kartlar = new int[52];
+        String[] deste = {"Sinek", "Kupa", "Karo", "Maça"};
+        String[] deger = {"As", "2", "3", "4", "5", "6", "7", "8", "9",
+        "10", "Oğlan", "Kız", "Papaz"};
+
+        for (int i = 0; i < kartlar.length; i++) {
+            kartlar[i] = i;
+        }
+
+        for (int i = 0; i < kartlar.length; i++) {
+            int damga = (int)(Math.random()*kartlar.length);
+            int gecici = kartlar[i];
+            kartlar[i] = kartlar[damga];
+            kartlar[damga] = gecici;
+        }
+
+        for (int i = 0; i < kartlar.length; i++) {
+            System.out.println(kartlar[i]);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            deste[i] = deste[kartlar[i]/13];
+            deger[i] = deger[kartlar[i]%13];
+            System.out.println("Kart: " + deste[i] + " " + deger[i]);
+        }
+    }
+}
