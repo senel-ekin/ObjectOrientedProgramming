05307125b9d94f588d7664c5a6c7a8f4deb49cf9
senel-ekin <s.ekin_ozekin@hotmail.com>
2023-12-24 18:43:10 +0300

    done

--- Array/src/array.java
+++ Array/src/array.java
@@ -1,3 +1,5 @@
+import static java.lang.System.arraycopy;
+
 public class array {
     public static void main(String[] args) {
         double[] array01 = new double[10];
@@ -8,13 +10,20 @@ public class array {
 
         System.out.println("");
         double[] array02 = new double[10];
-        array02 = array01;
+        //array02 = array01;
+
+        //for (int i = 0; i < array01.length; i++) {
+        //    array02[i] = array01[i];
+        //}
+
+        arraycopy(array01,2,array02,2,5);
+
         for (int i = 0; i < array01.length; i++) {
             System.out.format("%5.2f  ", array02[i]);
         }
 
         System.out.println("");
-        array01[5]=1.11;
+        //array01[5]=1.11;
         for (int i = 0; i < array01.length; i++) {
             System.out.format("%5.2f  ", array01[i]);
         }
